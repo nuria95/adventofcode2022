6d58ae82992ae572c633fae2cd6262aaef96e195
Nuria Armengol Urpi <nuria.armengolurpi@inf.ethz.ch>
2022-12-10 19:58:10 +0100

    small changes

--- day6/day6.py
+++ day6/day6.py
@@ -1,10 +1,10 @@
 with open("input.txt") as f:
 	data = f.read()
 
-nchars = 4 #14   
-for i in range(nchars, len(data)):
-    seq = data[i-nchars:i]
-    if len(set(seq)) == nchars:
-        print(i, seq)
-        break
+for part, nchars in zip(['part1', 'part2'], [4,14]): 
+    for i in range(nchars, len(data)):
+        seq = data[i-nchars:i]
+        if len(set(seq)) == nchars:
+            print('Solution ', part, i)
+            break
 
